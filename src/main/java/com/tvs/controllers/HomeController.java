11390339378a9dfc06b381b17a34388d559e8df3
root <root@localhost>
2018-07-08 16:29:57 +0200

    add controllers and change the package to scan and add applicationConfiguration plus initializer

---
+++ src/main/java/com/tvs/controllers/HomeController.java
@@ -0,0 +1,41 @@
+package com.tvsconsult.controller;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mobile.device.Device;
+import org.springframework.mobile.device.DeviceUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.async.WebAsyncTask;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.tvsconsult.services.AzureBlobStorage;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/aboutus")
+	public ModelAndView aboutUsF(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout) {
+
+		return new ModelAndView("aboutus", "message", "");
+	}
+		
+	@RequestMapping("/contacts")
+	public ModelAndView contactGet(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout) {
+
+		String s = "";
+		return new ModelAndView("contacts", "message", s);
+	}
+
+}
